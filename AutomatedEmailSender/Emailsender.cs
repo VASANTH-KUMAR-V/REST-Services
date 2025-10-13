c153e4d69c73cba835b9e8dd27245f928a1b1fad
VASANTH-KUMAR-V <vasanthvc02@gmail.com>
2025-10-13 13:15:12 +0530

    Created REST API Email Sender

---
+++ AutomatedEmailSender/Emailsender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AutomatedEmailSender
+{
+    public abstract class EmailSender
+    {
+        public string FromAddress;
+        public string ToAddress;
+        public string CcAddress;
+        public string BccAddress;
+        public string Subject;
+        public string Content;
+
+
+        public EmailSender(string fromaddress, string toaddress,string ccaddress,string bccaddress,  string subject, string content)
+        {
+            FromAddress = fromaddress;
+            ToAddress = toaddress;
+            CcAddress = ccaddress;
+            BccAddress = bccaddress;
+            Subject = subject;
+            Content = content;
+        }
+
+        public abstract void SendEmail();
+    }
+}
